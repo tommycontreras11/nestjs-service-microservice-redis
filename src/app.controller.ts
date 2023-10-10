43de6254b2b21322be8860d11ac858925a3d6812
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-10-10 11:33:35 -0400

    Added functionality to app controller to manage requests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { MessagePattern, EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  
+  @MessagePattern({ cmd: 'greeting' })
+  getGreetingMessage(name: string): string {
+    return `Hello ${name}`;
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'greeting-async' })
+  async getGreetingMessageAsync(name: string): Promise<string> {
+    return `Hello ${name} Async`;
+  }
+
+  @EventPattern('greeting-async')
+  async handleBookCreatedEvent(data: Record<string, unknown>) {
+    console.log(data);
   }
 }
